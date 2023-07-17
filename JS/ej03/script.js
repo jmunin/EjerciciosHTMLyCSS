09ba2f1935c985c389d6d0868510448230ec78fa
jose.munin <jose.munin@campusdual.com>
2023-07-17 09:50:25 +0200

    Añadido ejercicio JS 3

---
+++ JS/ej03/script.js
@@ -0,0 +1,14 @@
+//window.addEventListener('load', addTexto);
+
+let identificadorDeTemporizador;
+let current, horas, minutos, segundos;
+
+function mostrarHora() {
+    let elemento = document.getElementById("contenido");
+    current = new Date();
+    horas = current.getHours();
+    minutos = current.getMinutes();
+    segundos = current.getSeconds();
+    elemento.innerHTML = horas + ":" + minutos + ":" + segundos;
+    identificadorDeTemporizador = setTimeout(mostrarHora, 1000);
+}

74c5fedd365ad8305aa1b8c491384f38ac5d5ce8
jose.munin <jose.munin@campusdual.com>
2023-07-21 10:27:13 +0200

    Añadimos corrección ejercicio JS 3

--- JS/ej03/script.js
+++ JS/ej03/script.js
@@ -6,9 +6,11 @@ let current, horas, minutos, segundos;
 function mostrarHora() {
     let elemento = document.getElementById("contenido");
     current = new Date();
-    horas = current.getHours();
-    minutos = current.getMinutes();
-    segundos = current.getSeconds();
-    elemento.innerHTML = horas + ":" + minutos + ":" + segundos;
+    
+    // horas = current.getHours();
+    // minutos = current.getMinutes();
+    // segundos = current.getSeconds();
+    //elemento.innerHTML = horas + ":" + minutos + ":" + segundos;
+    elemento.innerHTML = current.toLocaleTimeString("es-ES");
     identificadorDeTemporizador = setTimeout(mostrarHora, 1000);
 }

fe36df4acafd9bd4f3354c19160b46528eba741b
jose.munin <jose.munin@campusdual.com>
2023-07-21 14:37:41 +0200

    Correcciones método removeSpecificNode

--- JS/ej06/script.js
+++ JS/ej06/script.js
@@ -4,14 +4,15 @@ const imagenes = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg"];
 function addImagen() {
     let elemento = document.getElementById("imagenes");
     let num_elementos = elemento.childNodes.length;
-    const numRandom = randomRangeNumber(1, 5);
+    const numRandom = randomRangeNumber(0, 4);
     if(num_elementos<5){
+    
         let img = document.createElement("img");
-        img.src = "assets/img" + numRandom + ".jpg"; // + imagenes[num_elementos];
-        img.id= imagenes[num_elementos]; //"img" + (elemento.childNodes.length + 1)
+        img.src = "assets/" + imagenes[numRandom]; // + imagenes[num_elementos];
+        img.id= imagenes[numRandom].slice(0,-4); //"img" + (elemento.childNodes.length + 1)
         img.width=126;
         img.height=189;
-        img.alt="Foto de perfil " + num_elementos;
+        img.alt="Foto de perfil " + imagenes[numRandom];
         elemento.appendChild(img);
     }   
 }
@@ -23,10 +24,10 @@ function delImagen() {
 }
 
 function removeSpecificNode(el, index) {
-    if (!Number.isNaN(index)) {
-        var children = el.children;
+    if(Number.isInteger(index)) {
+        const children = el.children;
         if(children.length > 0) {
-            if(children[index]!=null)
+            if(children[index]!=undefined)
                 el.removeChild(children[index]);
         }
     }
